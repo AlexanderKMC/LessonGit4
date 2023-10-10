dde13ceb1bdcb78283a3756837f6a95acbcf283a
AlexanderKMC <Smj500hsams@gmail.com>
2023-10-10 20:38:22 +0300

    add new system out println

--- src/Gitl4.java
+++ src/Gitl4.java
@@ -1,6 +1,7 @@
 public class Gitl4 {
     public static void main(String[] args) {
         System.out.println("Hello world and Git! I am Alexander");
+        System.out.println("second commit to git");
 
 
     }
